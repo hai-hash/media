079d2087432f0d94bdd67f2d8f1e983eb80cfbdf
whynotme1131999 <whynotme1131999@gmail.com>
2021-09-02 23:39:57 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './library/layout/layout';
+import Body from './page/body';
+
+function App() {
+  return (
+    <>
+    <Layout/>
+    <Body/>
+    
+    </>
+  );
+}
+
+export default App;
